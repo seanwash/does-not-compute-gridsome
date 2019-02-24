335ecf3e283733d1950f48e59070667872a18161
Sean Washington <hello@seanwash.com>
2019-02-23 22:29:51 -0800

    updates

--- gridsome.config.js
+++ gridsome.config.js
@@ -21,19 +21,5 @@ module.exports = {
         config: './tailwind.js',
       },
     },
-    {
-      use: 'gridsome-plugin-purgecss',
-      // default options, the following will be included if you don't provide
-      // anything
-      options: {
-        content: [
-          './src/**/*.vue',
-          './src/**/*.js',
-          './src/**/*.jsx',
-          './src/**/*.md',
-        ],
-        extensions: ['vue', 'js', 'jsx', 'md'],
-      },
-    },
   ],
 }
