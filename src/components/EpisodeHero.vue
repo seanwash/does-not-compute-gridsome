a98fa75bc5a58637678cacfef9fc262644a7fd5c
Sean Washington <hello@seanwash.com>
2019-03-25 23:33:43 -0700

    Updates

--- src/components/EpisodeHero.vue
+++ src/components/EpisodeHero.vue
@@ -2,34 +2,34 @@
   <article class="py-12 bg-grey-light">
     <Container>
       <header>
-        <time
-          :datetime="episode.publishedAt"
-        >{{ episode.publishedAt | localeDate }}</time>
+        <time :datetime="episode.publishedAt">{{
+          episode.publishedAt | localeDate
+        }}</time>
 
         <h1 class="text-3xl lg:text-5xl leading-tight my-2">
-          <g-link
-            class="no-underline text-black"
-            :to="episode.path"
-          >{{ episode.title }}</g-link>
+          <g-link class="no-underline text-black" :to="episode.path">{{
+            episode.title
+          }}</g-link>
         </h1>
       </header>
 
-      <p
-        class="text-xl lg:text-2xl leading-tight mt-4 mb-6 max-w-lg"
-      >{{ episode.description }}</p>
+      <p class="text-xl lg:text-2xl leading-tight mt-4 mb-6 max-w-lg">
+        {{ episode.description }}
+      </p>
 
       <div class="bg-white rounded shadow p-4">
-        <PlyrAudio :audio-url="episode.audioUrl"/>
+        <PlyrAudio :audio-url="episode.audioUrl" />
 
         <div class="mt-4 flex flex-col md:flex-row items-center">
           <g-link
             class="btn btn-adaptive-dark inline-block w-full md:w-auto flex-grow text-center"
             :to="episode.path"
-          >View show notes</g-link>
+            >View show notes</g-link
+          >
 
           <span class="hidden md:block mx-4">or</span>
 
-          <SocialButtonCollection class="hidden md:block"/>
+          <SocialButtonCollection class="hidden md:block" />
         </div>
       </div>
     </Container>
@@ -55,5 +55,4 @@ export default {
 }
 </script>
 
-<style>
-</style>
+<style></style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,29 +4,35 @@
       <PodcastHeader :heading="meta.heading"/>
     </template>
 
-    <ul class="pl-0 pt-12">
+    <ul class="flex flex-wrap -mx-2 pl-0 pt-10">
       <li
-        class="list-reset my-12"
         v-for="({ node }, index) in $page.episodes.edges"
-        :class="{ 'mt-0': index === 0 }"
-        :key="node.id"
+        class="flex w-full md:w-1/2 lg:w-1/3 p-2 list-reset"
       >
-        <episode-summary :episode="node"/>
+        <EpisodeSummary
+          :key="node.id"
+          :episode="node"
+          class="p-4 shadow rounded bg-grey-lightest"
+        />
       </li>
     </ul>
 
-    <div class="my-6">
+    <div class="mt-6 mb-12">
       <g-link
-        class="btn btn-grey block w-full text-center"
+        class="btn btn-adaptive-dark block w-full text-center"
         to="/episodes/2"
       >View More Episodes</g-link>
     </div>
+
+    <template slot="post-content">
+      <HostBar/>
+    </template>
   </Layout>
 </template>
 
 <page-query>
 query Episode {
-  episodes: allEpisode(sortBy: "publishedAt", perPage: 10) {
+  episodes: allEpisode(filter: { publishedAt: { lte: "now" }}, sortBy: "publishedAt", perPage: 12) {
     edges {
       node {
         id
@@ -45,11 +51,13 @@ query Episode {
 <script>
 import PodcastHeader from '~/components/PodcastHeader'
 import EpisodeSummary from '~/components/EpisodeSummary'
+import HostBar from '~/components/HostBar'
 
 export default {
   components: {
     PodcastHeader,
     EpisodeSummary,
+    HostBar,
   },
 
   data() {
@@ -87,4 +95,3 @@ export default {
   },
 }
 </script>
-
