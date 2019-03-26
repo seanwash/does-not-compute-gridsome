6e349f4a2f1805405ec973606f9cab030f952a0a
Sean Washington <hello@seanwash.com>
2019-03-25 19:47:39 -0700

    Remove dotenv call since Gridsome 0.5 supports it natively

--- gridsome.server.js
+++ gridsome.server.js
@@ -5,9 +5,6 @@
 // Changes here requires a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
-// TODO: Remove this once Gridsome 0.5 is released with built in .env support
-require('dotenv').config()
-
 const axios = require('axios')
 
 const PODCAST_ID = process.env.PODCAST_ID
@@ -16,7 +13,9 @@ const API_KEY = process.env.API_KEY
 
 module.exports = function(api) {
   api.loadSource(async store => {
-    const { data } = await axios.get(`${API_URL}/podcasts/${PODCAST_ID}/episodes.json?api_key=${API_KEY}`)
+    const { data } = await axios.get(
+      `${API_URL}/podcasts/${PODCAST_ID}/episodes.json?api_key=${API_KEY}`
+    )
 
     const contentType = store.addContentType({
       typeName: 'Episode',
