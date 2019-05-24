013c2ee9c76c51156ee0234401e16a0826a9f24a
Sean Washington <hello@seanwash.com>
2019-05-24 10:08:02 -0700

    Change audio_url protocol from http to https
    Mainly for silencing Netlify's mixed content warning emails.

--- gridsome.server.js
+++ gridsome.server.js
@@ -23,11 +23,27 @@ module.exports = function(api) {
     })
 
     for (const item of data) {
+      let fields = { ...item, audio_url: replaceHttp(item.audio_url) }
+
       contentType.addNode({
         id: item.id,
         title: item.title,
-        fields: { ...item },
+        fields: fields,
       })
     }
   })
+}
+
+/**
+ * Simplecast's API gives us `audio_url`s with an http protocol, which sets off
+ * Netlify's mixed content warning policy. This is a utility function for changing
+ * a URL instance's protocol from http to https.
+ *
+ * @param urlString
+ * @returns {string}
+ */
+function replaceHttp(urlString) {
+  const audioUrl = new URL(urlString)
+  audioUrl.protocol = 'https'
+  return audioUrl.href
 }
