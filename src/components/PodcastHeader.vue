60c289eb26a77098ba3db2981d4a8edfb26f24d6
Sean Washington <hello@seanwash.com>
2019-11-04 20:34:08 +0000

    🤯

--- src/components/PodcastHeader.vue
+++ src/components/PodcastHeader.vue
@@ -4,9 +4,9 @@
       <div class="w-1/3 hidden lg:block">
         <h1 class="text-6xl text-black m-0 leading-none">
           Does
-          <br>Not
-          <br>Compu
-          <br>te
+          <br />Not
+          <br />Compu
+          <br />te 🤯
         </h1>
       </div>
 
@@ -17,11 +17,11 @@
           class="text-3xl text-black font-normal leading-tight max-w-md mx-auto lg:mx-0 mb-6"
         >
           <strong>Does Not Compute</strong> is a weekly podcast about the lives and workflows of two indie web app developers.
-          <br>
-          <br>New episodes every Tuesday.
+          <br />
+          <br />New episodes every Tuesday.
         </h2>
 
-        <PodcastPlayerCollection/>
+        <PodcastPlayerCollection />
       </div>
     </Container>
   </div>
