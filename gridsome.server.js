5d807988d52b37fd7b70aa26933119cf99d791fd
Sean Washington <hello@seanwash.com>
2020-06-05 14:49:09 -0700

    Fix duplicate const declaration

--- gridsome.server.js
+++ gridsome.server.js
@@ -8,7 +8,6 @@
 const axios = require('axios')
 
 const PODCAST_ID = process.env.API_20_PODCAST_ID
-const PODCAST_ID = process.env.API_20_PODCAST_ID
 const API_URL = process.env.API_20_URL
 const API_KEY = process.env.API_20_KEY
 
